7ad49234beb9d738319b68b3970d3b58e5122879
Mehmet Nail Kocagoz <mehmetnailkocagoz@gmail.com>
2022-07-06 19:25:48 +0200

    06_07

--- src/test/java/BaseUrls.java
+++ src/test/java/BaseUrls.java
@@ -1,5 +1,18 @@
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.junit.Before;
+
 public class BaseUrls {
 
+
+    public RequestSpecification spec;
+
+    // Before annotation i kullandigimiz medhotlar her Test metodhodundan once kullanilir
+    @Before
+    public void  setUp(){
+
+        spec = new RequestSpecBuilder().setBaseUri("https://jsonplaceholder.typicode.com/").build();
+    }
 
 
 }
--- src/test/java/Get03.java
+++ src/test/java/Get03.java
@@ -1,4 +1,11 @@
-public class Get03 {
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class Get03 extends BaseUrls {
 
 
     //  Given
@@ -17,8 +24,35 @@ public class Get03 {
 	//	    “userId” is 2
 
 
+    @Test
+    public void get01(){
+        //1. step : Set the Url
+        //String url = "https://jsonplaceholder.typicode.com/todos/23"; // onerilmiyor
 
 
+        spec.pathParams("first","todos","second",23);
 
+        //2.step : set the expected data
 
+        //3. step : send the request and get the response
+
+        Response response = given().spec(spec).when().get("/{first}/{second}");
+
+        //4. step : do assertion
+
+        //1. yazim sekli
+        response.then().assertThat().statusCode(200)
+                .contentType("application/json")
+                .body("title",equalTo("et itaque necessitatibus maxime molestiae qui quas velit"))
+                .body("completed",equalTo(false))
+                .body("userId",equalTo(2));
+
+        //2. yazim sekli
+        response.then().assertThat().statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("title",equalTo("et itaque necessitatibus maxime molestiae qui quas velit")
+                ,"completed",equalTo(false)
+                ,"userId",equalTo(2));
+
+    }
 }
